512677c5635d24689460e863b79c1131d495ce3d
Theo P <teddypeifer@gmail.com>
2018-08-31 18:51:18 +0200

    Create main_class.java

---
+++ main_class.java
@@ -0,0 +1,44 @@
+import FileService.fileclient;
+import FileService.fileserver;
+
+import javax.swing.*;
+
+public class run {
+
+    public static void main(String[] agrs) throws Exception {
+
+        run frst = new run();
+        frst.run_frst();
+
+    }
+
+    /* FIRST SERVER */
+    public void run_frst() throws Exception{
+
+        String[] options = new String[] {"Server", "Client"};
+        int response = JOptionPane.showOptionDialog(null, "Start Client or Server?", "Choose",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        switch (response){
+            case(0):
+                myServer();
+                break;
+            case(1):
+                myClient();
+                break;
+            case(-1):
+                System.exit(0);
+        }
+    }
+
+    private static void myServer() throws Exception{
+        server s = new server();
+        s.Server();
+    }
+
+    private static void myClient() throws Exception{
+        client c = new client();
+        c.Client();
+    }
+}
